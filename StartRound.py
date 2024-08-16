80b42e014f54fb90b950aed0d429783d661519ea
Thomas Nast <thomas.nast23@gmail.com>
2024-08-16 16:34:17 -0700

    Remove action from StartRound

--- StartRound.py
+++ StartRound.py
@@ -2,7 +2,7 @@ import random
 from Rules import *
 from User import Player
 from GameMessage import *
-from enum import Enum
+from Action import Action
 #import argparse
 
 # this is not working with VSCode
@@ -11,13 +11,10 @@ from enum import Enum
 # #parser.add_argument("", type=int, help="Starting quantity of dice")
 # args = parser.parse_args()
 
-class Action(Enum):
-    INC_BID = 0
-    CALL_LIE = 1
     
 class Game:
 
-    def __init__(self, player_ct: int, dice_per: int):
+    def __init__(self, player_ct: int, dice_per: int, is_training = 0):
         self.player_ct = player_ct
         self.dice_per = dice_per
         self.players = []
@@ -28,6 +25,7 @@ class Game:
         self.dice_totals = {}        
         self.game_over = 0
         self.last_action = Action.INC_BID
+        self.is_training = is_training
         self.initialize_game()        
 
     def initialize_game(self):
@@ -47,52 +45,62 @@ class Game:
             self.players[p].roll()
         # track the total dice
         self.sum_dice()
-
         self.active_player = cur = self.players[0]
-        res = cur.place_bet(0, 0)   
-        self.update_bet_history(res)
-        print(self.bet_history)
-        self.next()
-        self.continue_round(self.last_bet()[0], self.last_bet()[1])
-    
-    # TODO: Split into smaller functions and DRY
-    # Want messages class to handle responses
-    def continue_round(self, last_qty: int, last_dice_val: int):
-        # move to next player
-        cur = self.players[self.next()]
-        res = cur.choose_bet_type(self.last_bet()[0], self.last_bet()[1])
-        
-        # if current player called the last one a liar
-        if res is None:
-            self.last_action = Action.CALL_LIE
-            q,f = self.last_bet()
-            if self.dice_totals.get(f) is None:
-                self.active_player = self.players[self.prev()]
-                self.active_player.remove_die()
-                #display_incorrect_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
-                self.start_new_round()          
-            if self.dice_totals[f] >= q:
-                self.active_player = self.players[self.prev()]
-                self.active_player.remove_die()
-                #display_incorrect_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
-                self.start_new_round()                
-            else:
-                #display_correct_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
-                self.active_player = self.players[self.prev()]
-                self.active_player.remove_die()
-                if self.active_player.dice == 0:
-                    self.remove_player(self.active_player)
-                self.start_new_round()
-        else:
-            self.last_action = Action.INC_BID
+        if self.is_training == 0:
+            res = cur.place_bet(0, 0)   
             self.update_bet_history(res)
             self.next()
+            self.continue_round()
+
+    # TODO: Split into smaller functions and DRY
+    # Want messages class to handle responses
+    def continue_round(self):
+        # move to next player
+        cur = self.players[self.next()]
+        if self.is_training == 0:
+            raised_to = cur.choose_bet_type(self.last_bet()[0], self.last_bet()[1])
+            
+            # if current player called the last one a liar
+            if raised_to is None:
+                self.resolve_liar_call()
+            else:
+                self.update_bet_history(raised_to)
+
+    # training implementation:
+
+    def set_bid(self, q, f):
+        lqf = self.last_bet()
+        assert is_valid_bet(qty=q, face_val=f, last_qty=lqf[0], last_face_val=lqf[1])
+        self.update_bet_history((q,f))
+
+    def resolve_bid(self, raised_to):
+            self.update_bet_history(raised_to)
+            self.next()
             self.continue_round(self.last_bet()[0], self.last_bet()[1])
+
+    def resolve_liar_call(self):
+        q,f = self.last_bet()
+        if self.dice_totals.get(f) is None:
+            self.active_player = self.players[self.prev()]
+            self.active_player.remove_die()
+            #display_incorrect_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
+            self.start_new_round()          
+        if self.dice_totals[f] >= q:
+            self.active_player = self.players[self.prev()]
+            self.active_player.remove_die()
+            #display_incorrect_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
+            self.start_new_round()                
+        else:
+            #display_correct_liar_call(self.active_player, self.players[self.look_prev()], self.dice_totals, f)
+            self.active_player = self.players[self.prev()]
+            self.active_player.remove_die()
+            if self.active_player.dice == 0:
+                self.remove_player(self.active_player)
+            self.start_new_round()
 
     def next(self):
         self.IDX = (self.IDX + 1) % self.player_ct
         return self.IDX
-
 
     # don't update the index
     def look_prev(self):
@@ -144,8 +152,8 @@ class Game:
     def end_game(self):
         print(f'{self.players[0]} wins.')
 
-    # represent the dice that player cannot see as an array of ints
-    def hidden_dice(self, player):
+    # represent the dice that player cannot see as an array of ints representing how many dice they have
+    def hidden_dice(self):
         hd = []
         for p in self.players:
             if self.active_player != p:
@@ -153,4 +161,4 @@ class Game:
         return hd
 
 # start a game with 3 people and 5 dice
-Game(3, 5)
+#Game(3, 5, 0)
