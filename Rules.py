4ed83ef28d67ceebc695fa0165bd237863cb0ba7
Thomas Nast <thomas.nast23@gmail.com>
2025-04-04 16:00:24 -0700

    Update rules to use new action

--- Rules.py
+++ Rules.py
@@ -1,15 +1,21 @@
 import torch
 # should I specify that the bid qty is not to exceed the number of dice in the game?
-def is_valid_bet(qty, last_qty, face_val, last_face_val):
+def is_valid_bet(new_action, last_action):
     # if this is the first bet, it is valid provided it's not calling someone a liar
-    if (last_qty == 0):
-        return True
-    if (qty < last_qty):
-        return False
-    if (qty == last_qty and face_val <= last_face_val):
-        return False
-    if (qty == last_qty and face_val > last_face_val):
-        return True
-    if (qty > last_qty):
-        return True
-    return True   
+    if last_action is None:
+        if new_action.is_bid():
+            return True
+        else:
+            return False
+    
+    if not last_action.is_bid():
+        return False # if the last action wasn't a bid, it was a call, so there ought to be nothing to do
+
+    last_bid = last_action.bid
+    new_bid = new_action.bid
+
+    if new_bid.quantity < last_bid.quantity:
+        return False # if the new bid has less quantity than the last bid, it is invalid
+    if new_bid.quantity == last_bid.quantity and new_bid.face_value <= last_bid.face_value:
+        return False # if the new bid has the same quantity as the last bid, it must have a higher face value
+    return True
