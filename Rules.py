4049105463603b9e6aa8155fdb43e8e256d7c441
Thomas Nast <thomas.nast23@gmail.com>
2025-04-04 15:34:07 -0700

    Fix bug comparing last_qty to last_qty

--- Rules.py
+++ Rules.py
@@ -1,8 +1,12 @@
 import torch
+# should I specify that the bid qty is not to exceed the number of dice in the game?
 def is_valid_bet(qty, last_qty, face_val, last_face_val):
+    # if this is the first bet, it is valid provided it's not calling someone a liar
+    if (last_qty == 0):
+        return True
     if (qty < last_qty):
         return False
-    if (qty == last_qty and last_face_val <= last_face_val):
+    if (qty == last_qty and face_val <= last_face_val):
         return False
     if (qty == last_qty and face_val > last_face_val):
         return True
