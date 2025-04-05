c2d7de16a53c15fc2101e42f3f96e3908bff4c51
Thomas Nast <thomas.nast23@gmail.com>
2025-04-04 19:03:02 -0700

    Refactor to use new action / bid structure

--- aigame.py
+++ aigame.py
@@ -1,104 +1,151 @@
 import random
-from Rules import *
-from AI import Agent
-from Action import *
+from Rules import is_valid_bet
+from AI import AI
+from Action import Action, ActionType, Bid
 import itertools
-from const import * 
-#import argparse
+from const import *
+from typing import List, Dict, Optional, Tuple, Any
+from dataclasses import dataclass
+from GameState import GameState
 
-# this is not working with VSCode
-# parser = argparse.ArgumentParser()
-# parser.add_argument("q", type=int, help="Starting quantity of dice")
-# #parser.add_argument("", type=int, help="Starting quantity of dice")
-# args = parser.parse_args()
-
-    
 class AIGame:
 
     def __init__(self, player_ct: int, dice_per: int):
         self.player_ct = player_ct
         self.dice_per = dice_per
-        self.players = []
+        self.players: List[AI] = []
         self.IDX = 0     
-        self.active_player = None # index of player who is betting
-        self.bet_history = []
-        self.dice_totals = {}        
+        self.active_player: Optional[AI] = None
+        self.game_state = GameState(
+            action_history=[],
+            player_dice_counts=[dice_per] * player_ct,
+            current_player=0,
+            total_dice=player_ct * dice_per,
+            dice_totals={},
+            bet_history=[]
+        )
         self.game_over = 0
-        self.last_action = Action.NONE
-        self.pub_state = []
-        self.initialize_game()        
+        self.initialize_game()
 
     def initialize_game(self):
+        """Initialize the game state"""
         self.make_players()
         self.set_pub_state()
-        #self.start_round()
 
     def make_players(self):
-        if(self.player_ct < 2):
+        """Create AI players for the game"""
+        if self.player_ct < 2:
             self.player_ct = 2
         for p in range(self.player_ct):                            
-            self.players.append(Agent(p))
+            self.players.append(AI(p))
 
     def start_round(self):
-        self.reset_bet_history()
-        self.reset_dice_totals()
+        """Start a new round of the game"""
+        self.game_state.bet_history = []
+        self.game_state.dice_totals = {}
         self.game_over = 0        
         for p in range(self.player_ct):
-            p.roll()
+            self.players[p].roll()
         self._sum_dice()
         self.active_player = self.players[0]
-        self.set_pub_state()
-        while self.game_over == 0:
-            self.step()
+        self.IDX = 0
+        self.game_state.current_player = 0
+        return self.set_pub_state()
         
-    def step(self):
-        raised_to = self.active_player.get_action(self.last_bet()[0], self.last_bet()[1])       
-        # if current player called the last one a liar
-        if raised_to is Action.CALL_LIE:
-            self.apply_liar_call()
-        else:
-            self.apply_bid(raised_to)
+    def step(self) -> Action:
+        """Execute one step of the game.
+        """
+        action = self.active_player.get_action(self.game_state.to_public_state())
+        if action.is_bid():
+            return self.apply_bid(action)
+        elif action.is_call_liar():
+            return self.apply_liar_call(action)
+        raise ValueError(f"Invalid action type: {action.type}")
 
-    def apply_bid(self, raised_to):
-        self.update_bet_history(raised_to)
-        self.update_opp_hist(raised_to)
+    # apply bid action to the game state
+    def apply_bid(self, action: Action) -> Action:
+
+        current_state = self.game_state.to_public_state()
+        
+        self.game_state.add_action(action)
+        self.update_opp_hist((action.bid.quantity, action.bid.face_value))
+        
+        prev_player = self.active_player
         self.active_player = self.players[self.next()]
-        return self.pub_state
+        self.game_state.current_player = self.IDX
+        
+        prev_player.update_q_value(current_state, action, prev_player.reward, self.game_state.to_public_state())
+        
+        return action
 
-    def apply_liar_call(self):
-        q,f = self.last_bet()
-        if self.dice_totals.get(f) is None:
-            self.active_player.reward += self.active_player.discount * 5            
-            self.active_player = self.players[self.prev()]
-            self.active_player.remove_die()
-        if self.dice_totals[f] >= q:
-            self.active_player.reward += self.active_player.discount * 5
-            self.active_player = self.players[self.prev()]
-            self.active_player.reward -= self.active_player.discount * 5
-            self.active_player.remove_die()
+    # apply liar call action to the game state
+    def apply_liar_call(self, action: Action) -> Action:
+        current_state = self.game_state.to_public_state()
+        last_bid = self.game_state.get_last_bid()
+        if not last_bid:
+            raise ValueError("Cannot call liar when there are no bids")
+            
+        caller = self.active_player
+        previous_player = self.players[self.look_prev()]
+        
+        actual_count = self.game_state.dice_totals.get(last_bid[1], 0)
+        bid_difference = last_bid[0] - actual_count
+        # If neither player had any of the faces bid upon, the caller received a large reward.
+        if self.game_state.dice_totals.get(last_bid[1]) is None:
+            caller.update_reward(15)
+            previous_player.update_reward(-15)
+            previous_player.remove_die()
+            self.game_state.player_dice_counts[self.look_prev()] -= 1
+            self.game_state.total_dice -= 1
+            self.active_player = previous_player
+        # If the caller was incorrect, adjust reward based on how incorrect they were.    
+        elif self.game_state.dice_totals[last_bid[1]] >= last_bid[0]:
+            reward = -5 - (actual_count - last_bid[0])
+            caller.update_reward(reward)
+            previous_player.update_reward(-reward)
+            caller.remove_die()
+            self.game_state.player_dice_counts[self.IDX] -= 1
+            self.game_state.total_dice -= 1
+            self.active_player = previous_player
+        #If the caller was correct, reward based on how closely they called it.
         else:
-            self.players[self.look_prev()].reward  += self.players[self.look_prev()].discount * 5
-            self.active_player.remove_die()
-            self.active_player.reward -= self.active_player.discount * 5
-            if self.active_player.dice == 0:
-                self.active_player.reward = -10
-                self.remove_player(self.active_player)
-        self.pub_state.append(LIE_TUPLE)
-        return self.pub_state
-    
-    def next(self):
+            reward = 5 + (10 / bid_difference + 1)
+            caller.update_reward(reward)
+            previous_player.update_reward(-reward)
+            previous_player.remove_die()
+            self.game_state.player_dice_counts[self.look_prev()] -= 1
+            self.game_state.total_dice -= 1
+            if previous_player.NUMDICE == 0:
+                previous_player.update_reward(-10)
+                self.remove_player(previous_player)
+            self.active_player = caller
+            
+        self.game_state.add_action(action)
+        caller.update_q_value(current_state, action, caller.reward, self.game_state.to_public_state())
+        if previous_player in self.players:
+            previous_player.update_q_value(current_state, Action.make_bid(*last_bid), previous_player.reward, self.game_state.to_public_state())
+        
+        return action
+    # Moves to next player and return index
+    def next(self) -> int:
+            
         self.IDX = (self.IDX + 1) % self.player_ct
         return self.IDX
 
-    # don't update the index
-    def look_prev(self):
+    # Get the index of the previous player without updating the current index.
+    def look_prev(self) -> int:
         tmp = (self.IDX - 1)
-        if tmp < 0 : tmp = self.player_ct -1 
+        if tmp < 0:
+            tmp = self.player_ct - 1 
         return tmp
-    # does update the index
-    def prev(self):
+
+    # Move to the previous player and return index.
+    def prev(self) -> int:
         self.IDX = (self.IDX - 1)
-        if self.IDX < 0 : self.IDX = self.player_ct -1 
+
+        self.IDX = (self.IDX - 1)
+        if self.IDX < 0:
+            self.IDX = self.player_ct - 1 
         return self.IDX
 
     def _all_rolls(self, player):
@@ -110,62 +157,74 @@ class AIGame:
         ]
 
     def _sum_dice(self):
+        """Sum up all dice in play."""
+        self.game_state.dice_totals = {}
         for p in range(self.player_ct):
             for d in self.players[p].rolls:
-                if self.dice_totals.get(d) is None:
-                    self.dice_totals[d] = 1
+                if self.game_state.dice_totals.get(d) is None:
+                    self.game_state.dice_totals[d] = 1
                 else:
-                    self.dice_totals[d] += 1
+                    self.game_state.dice_totals[d] += 1
     
-    def update_bet_history(self, qv):
-        self.bet_history.append(qv)
-        self.pub_state.append(qv[0])
-        self.pub_state.append(qv[1])
-        self.pub_state[BET_HIST_IDX] += 1
-    
-    def update_opp_hist(self, qf):
+    # Update opponent bid history for learning.
+    # TODO: This should be made consistent with the game state implementation.
+    def update_opp_hist(self, qf: Tuple[int, int]):
+        """Update opponent bid history for learning.
+        
+        Args:
+            qf: Tuple of (quantity, face_value) for the bid
+        """
         for p in self.players:
             if p != self.active_player:
                 p.update_opp_bids(qf)
 
     def reset_bet_history(self):
-        self.bet_history = []
-        self.pub_state[BET_HIST_IDX] = 0
+        """Reset the bet history for a new round."""
+        self.game_state.bet_history = []
 
     def reset_dice_totals(self):
-        self.dice_totals = {}
+        """Reset the dice totals for a new round."""
+        self.game_state.dice_totals = {}
 
-    def last_bet(self):
-        if len(self.bet_history) > 0:
-            return self.bet_history[-1]
+    # Get the last bet made in the game. Convenience function.
+    def last_bet(self) -> Optional[Tuple[int, int]]:
+        if len(self.game_state.bet_history) > 0:
+            return self.game_state.bet_history[-1]
+        return None
 
-    def check_game_over(self):
-        return self.players.count == 1
+    # Check if the game is over.
+    def check_game_over(self) -> bool:
+        return len(self.players) == 1
       
-    def remove_player(self, player):
+    def remove_player(self, player: AI):
+        """Remove a player from the game.
+        
+        Args:
+            player: The player to remove
+        """
+        player_idx = self.players.index(player)
         self.players.remove(player)
-        self.player_ct -= 1 
+        self.player_ct -= 1
+        self.game_state.player_dice_counts.pop(player_idx)
         
         if self.check_game_over():
             self.end_game()
         else:
-            self.set_turn_order() # Reset the turn order now that there is one fewer player
-
+            # Reset IDX if it's now out of bounds
+            if self.IDX >= self.player_ct:
+                self.IDX = 0
+    # TODO: Update rewards for winner, this should trigger some persistence.
     def end_game(self):
         self.game_over = 1
-        print(f'{self.players[0]} wins.')
+        print(f'{self.players[0].name} wins!')
 
-    def set_pub_state(self):
-        self.pub_state = [len(self.bet_history), self.player_ct, self.last_action]
-        dice_ct = 0
-        for x in self.players:
-            dice_ct += x.NUMDICE
-        self.pub_state.append(dice_ct)
-        self.pub_state.append(self.hidden_dice())
-    
-    # represent the dice that player cannot see as an array of ints representing how many dice they have
-    def hidden_dice(self):
-        hd = []        
-        for p in self.players:
-            hd.append(p.NUMDICE)
-        return hd
+    def set_pub_state(self) -> List[Any]:
+        """Set the public state of the game.
+        
+        Returns:
+            List[Any]: The public state
+        """
+        return self.game_state.to_public_state()
+    # Get opponents dice counts    
+    def hidden_dice(self) -> List[int]:
+        return [p.NUMDICE for p in self.players if p != self.active_player]
--- train_step.py
+++ train_step.py
@@ -3,8 +3,11 @@
 # import torch.optim as optim
 # import torch.nn.functional as F
 from aigame import AIGame
-from AI import *
-from Action import *
+from AI import AI
+from Action import Action, ActionType
+import os
+import time
+from typing import List
 #import numpy as np
 # state = [0,5,3,0,5,5,5,6,6,5,3,1]
 # q_table = {(0,1): .2, (0,0): .3, (1,0) : .2, (1,1) : .8}
@@ -20,17 +23,41 @@ from Action import *
     # action = torch.tensor(action, dtype=torch.long)
     # reward = torch.tensor(reward, dtype=torch.float)
 
-def train():
-    game = AIGame(2,5)
-    agents = game.players
-    agent = agents[0]
-    pub_state = game.pub_state
-    while game.player_ct > 1:
-        #prev_state = agent.get_state(game)
-        bid = agent.get_action(pub_state)
-        if bid == LIE_TUPLE:
-            game.start_round()
+def write_bids_to_file(bid_hist: List[str], filename: str = f'test{time.time()}.txt'):
+    """Write bid history to a file.
+    
+    Args:
+        bid_hist: List of bid history strings
+        filename: Name of the file to write to
+    """
+    if os.path.isdir('test'):
+        with open(f'test/{filename}', 'w+') as f:
+            f.writelines([i + '\n' for i in bid_hist])
+    else:
+        print('Test directory not found')
 
+def train():
+    """Train AI agents by playing games."""
+    game = AIGame(2, 5)
+    agents = game.players   
+    pub_state = game.pub_state
+    game.start_round()
+    agent = game.active_player
+    hist = []
+    i = 0
+    max_bids = 10
+    while game.player_ct > 1 and i < max_bids:
+        action = game.step()
+        i += 1
+        hist.append(f'{game.active_player.name} {action}')
+        
+        if action.is_call_liar():
+            game.start_round()
+        else:
+            game.step()
+            
+    write_bids_to_file(hist, '1.txt')
+    
 if __name__ == '__main__':
     train()
 
