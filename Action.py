40d469ce99d4f4bee2168fa767878858c52e57e3
Thomas Nast <thomas.nast23@gmail.com>
2025-04-13 19:06:07 -0700

    tuples

--- Action.py
+++ Action.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from dataclasses import dataclass
-from typing import Optional
-from const import MAX_DICE_PER_PLAYER
+from typing import Optional, Tuple
+from const import MAX_TOTAL_DICE
 
 class ActionType(Enum):
     BID = 0
@@ -12,19 +12,32 @@ class ActionType(Enum):
 class Bid:
     quantity: int
     face_value: int
+    def __init__(self, quantity: int, face_value: int):
+        self.quantity = quantity
+        self.face_value = face_value
 
     def __post_init__(self):
-        if not (1 <= self.quantity <= MAX_DICE_PER_PLAYER):  # Reasonable max for total dice
-            print(f"Invalid quantity: {self.quantity}")
+        if not (1 <= self.quantity <= MAX_TOTAL_DICE):
             raise ValueError(f"Invalid quantity: {self.quantity}")
-        if not (1 <= self.face_value <= NUMBER_FACES):
+        if not (1 <= self.face_value <= 6):
             raise ValueError(f"Invalid face value: {self.face_value}")
 
+    def to_tuple(self) -> Tuple[int, int]:
+        return (self.quantity, self.face_value)
+
     def __lt__(self, other: 'Bid') -> bool:
-        return self.quantity < other.quantity or (self.quantity == other.quantity and self.face_value < other.face_value)
+        if self.quantity != other.quantity:
+            return self.quantity < other.quantity
+        return self.face_value < other.face_value
 
     def __gt__(self, other: 'Bid') -> bool:
-        return self.quantity > other.quantity or (self.quantity == other.quantity and self.face_value > other.face_value)
+        if self.quantity != other.quantity:
+            return self.quantity > other.quantity
+        return self.face_value > other.face_value
+
+    def __eq__(self, other: 'Bid') -> bool:
+        return self.quantity == other.quantity and self.face_value == other.face_value
+
 
 # Add a second action class that contains action and Bid.
 @dataclass(frozen=True)
@@ -45,7 +58,13 @@ class Action:
 
     def is_call_liar(self) -> bool:
         return self.type == ActionType.CALL_LIE
-
+    # convert to tuple for Q-table
+    def to_tuple(self) -> Tuple[ActionType, Optional[Bid]]:
+        if(self.is_bid()):
+            return (self.type, self.bid)
+        else:
+            return (self.type, None)
+    # make readable for debugging / logging
     def __str__(self) -> str:
         if self.is_bid():
             return f"Action.make_bid({self.bid.quantity}, {self.bid.face_value})"
@@ -56,9 +75,8 @@ class Action:
 
     # make hashable for Q-table
     def __hash__(self) -> int:
-        print("hashing action")
         if self.is_bid():
-            return hash((self.type, self.bid.quantity, self.bid.face_value))
+            return hash((self.type, self.bid.to_tuple()))
         return hash((self.type,))
 
     def __eq__(self, other: object) -> bool:
