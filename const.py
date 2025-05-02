7acc1c0035a0dfdb7d43c66bad39a0860f5d2f7f
Thomas Nast <thomas.nast23@gmail.com>
2025-05-02 13:55:15 -0700

    Add model path constants.

--- const.py
+++ const.py
@@ -9,24 +9,24 @@ MAX_DICE_PER_PLAYER = 5 # if it can go up, even better.
 # The state is represented as a 2D list where each row is a different component
 # Each row is prefixed with its length for tensor conversion
 STATE_COMPONENTS = {
-    'BET_HISTORY': 0,
-    'PLAYER_INFO': 1,
-    'LAST_ACTION': 2,
-    'DICE_COUNTS': 3,      # [length, player1_dice, player2_dice, ...]
-    'CLUSTER_INFO': 4,     # [length, method, count, current_cluster]
-    'LAST_BID': 5,         # [length, quantity, face_value]
-    'CLUSTER_MGR': 6       # [length, cluster_manager_ref]
+    'PLAYER_IDX': 0,
+    'HANDS': 1,      # [length, player1_dice, player2_dice, ...]
+    'MOST_FREQ_OPP_BID': 2,         # [length, quantity, face_value]
+    'LAST_BID': 3,       # [length, cluster_manager_ref]
+    'PLAYERS': 2,
 }
-
+FEATURE_WEIGHTS = {
+    'PLAYER_IDX': 0.1,
+    'HANDS': 0.9,
+    'MOST_FREQ_OPP_BID': 2.0,
+    'LAST_BID': 0.1,
+    'PLAYERS': 0.1,
+}
 # Maximum lengths for each component
-MAX_BET_HISTORY = 12
 MAX_PLAYERS = 6
 MAX_ACTIONS = 1
 MAX_DICE_COUNTS = MAX_DICE_PER_PLAYER
-MAX_CLUSTER_INFO = 4
 MAX_LAST_BID = 2
-MAX_CLUSTER_MGR = 1
-MAX_PLAYERS = 8
 
 # Game constants
 MAX_FACE_VALUE = 6
@@ -34,21 +34,15 @@ MIN_FACE_VALUE = 1
 MIN_PLAYERS = 2
 
 # State approximation constants
-NUM_CLUSTERS = 100
-
+MEMORY_SIZE = 1000
 # Approximation constants
-QTY_DISTANCE_WEIGHT = 0.4
-FACE_DISTANCE_WEIGHT = 0.3
-QTY_RATIO_DISTANCE_WEIGHT = 0.2
-HISTORY_LENGTH_DISTANCE_WEIGHT = 0.1
-
 MAX_ROUND_MEMORY = 4
 NUMBER_FACES = 6
 
 MAX_DIE_PER_PLAYER = 5
-
+RANDOM_LIAR_PROB = 0.2
 # AI constants
-EPSILON = 0.5
+EPSILON = 1
 DISCOUNT_FACTOR = 0.9
 LEARNING_RATE = 0.1
 
@@ -59,4 +53,7 @@ MAX_DIE_BONUS_WEIGHT = 2
 PUB_STATE_SIZE = 10
 MAX_TOTAL_DICE = MAX_PLAYERS * MAX_DICE_PER_PLAYER
 # Data constants
-CLUSTER_CENTERS_FILE = "./data/cluster_centers.json"
+CUR_Q_TABLE_NAME = "KidGloves2"
+CUR_MODEL_NAME = "KidGloves2_model.pt"
+CUR_MODEL_PATH = "models/deep_q/" + CUR_MODEL_NAME
+CUR_MODEL_ONNX_PATH = "models/deep_q/" + CUR_MODEL_NAME + ".onnx"
