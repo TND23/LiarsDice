bbe2af91bb7025861ca34af0c2e60663297a14b3
Thomas Nast <thomas.nast23@gmail.com>
2025-05-12 16:03:03 -0700

    Simplify reward structure

--- Robot/AI.py
+++ Robot/AI.py
@@ -26,15 +26,12 @@ class AI:
         self.state_manager = StateManager()
         self.persistence = QTablePersistence()
         self.action_manager = ActionManager()
-        self.spots_could_have_called_liar = []
 
     def get_action(self, state_manager: StateManager) -> Action:
         """Get the next action based on the current state."""
         # might be uneccessary to update state_manager
         self.state_manager = state_manager
         valid_actions = self.action_manager.get_valid_actions(state_manager)
-        if valid_actions.count(Action.call_liar()) > 0:
-            self.spots_could_have_called_liar.append(state_manager.get_game_state().last_bid)
         action_values = {}
         state_key = self.state_manager.get_state_key()
         # Initialize Q-values for valid actions
@@ -56,7 +53,6 @@ class AI:
             chosen_action = random.choice(valid_actions)
         else:
             chosen_action = max(action_values.items(), key=lambda x: x[1])[0]
-
         return chosen_action
     #TODO: this doesn't work without a proper next state.
     def update_q_value(self, action: Action, reward: float) -> None:
@@ -102,45 +98,45 @@ class AI:
         bid_probability = self.calculate_bid_probability(game_state.get_last_bid())
         had_other_actions = self.action_manager.get_valid_actions(state_manager) != [Action.call_liar()]
         opponent_dice_count = sum(len(hand) for hand in [i_hand for i, i_hand in enumerate(game_state.hands) if i != self.p_index])
+
         """ Trying to prevent really stupid behavior for now."""
         if enforcement_type == "reward_liar_call":
+            base_reward = 50
             # if the player can prove the bid is a lie, large reward.
             if bid_probability == 0.0:
                 # The caller was correct - the bid was impossible
-                self.reward += 30
-            # if the player sees that the bid is possible but that it is not the most frequent opponent bid, reward.
-            else:
-                self.reward += min(1/bid_probability,29.9)
-            # Base reward for making a correct liar call
-            if bid_probability > 0.2: # extra reward for extra unlikely events
-                self.reward += 5
-            if (last_bid_qty / self._total_dice()) > 0.5: # extra reward for high bids
-                self.reward += 2
-            self.reward += 2.5
+                self.reward += base_reward * 2
+            elif bid_probability < 0.2:
+                self.reward += base_reward * 1.5
+            elif bid_probability < 0.5:
+                self.reward += base_reward
+
         elif enforcement_type == "punish_liar_call":
+            base_penalty = -40
             # if the player called liar but the bid was provably true, punish harshly.
             if bid_probability == 1.0:
-                self.reward -= 20
+                self.reward -= base_penalty * 2
             # if the player can reason that the bid is probably true and calls a liar, punish
             elif bid_probability > 0.5 and had_other_actions and most_freq_opp_face != last_bid_face:
-                self.reward -= 8
+                self.reward -= base_penalty * 1.5
             # if the player can reason that the bid is probably true and calls a liar, punish
             elif bid_probability > 0.5 and had_other_actions == False:
-                self.reward -= 1
-            # if the bid face matches the most frequent opponent face, punish for calling liar
-            if bid_probability > 0.5 and most_freq_opp_face == last_bid_face and had_other_actions:
-                self.reward -= 10
-            elif bid_probability < 0.5:
-                self.reward -= 4
+                self.reward -= base_penalty
         elif enforcement_type == "reward_passive":
             # Reward for not making a bad liar call
-            self.reward += 5
+            if bid_probability > 0.5:
+                self.reward += 20
+            elif bid_probability > 0.3:
+                self.reward += 10
         elif enforcement_type == "punish_passive":
-            if bid_probability < 0.5:
-                self.reward -= 15
-            else:
-                self.reward -= 8
+            if bid_probability < 0.2:
+                self.reward -= 40
+            elif bid_probability < 0.4:
+                self.reward -= 20
 
+
+    def update_reward_from_history(self, enforcement_type: str, state_manager: StateManager, round_history: List[Action]) -> None:
+        pass
     #region helpers
     def get_expected_number_of_opponent_face(self, opponent_dice_count: int) -> int:
         return round(opponent_dice_count / NUMBER_FACES)
